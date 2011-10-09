85e472bb92785bc9e11704ad7abc4023ab93f56c
Ryan Johnston <mrryanjohnston@gmail.com>
2011-10-09 16:51:52 -0400

    Added more functionality to the Character class. Breaking to read about TDG design and to figure out a good way to access Characters from the DB

--- classes/Character.php
+++ classes/Character.php
@@ -42,7 +42,42 @@ class Character
 
   private $dead = NULL;
 
-  /*Getter/Setter as found on SO: http://stackoverflow.com/questions/4478661/getter-and-setter */
+  function __construct($attributes) {
+    $this->userID = $attributes['userID'];
+    $this->characterID = $attributes['characterID'];
+    $this->name = $attributes['name'];
+    $this->gender = $attributes['gender'];
+
+    $this->head = $attributes['head'];
+    $this->hair = $attributes['hair'];
+    $this->eyes = $attributes['eyes'];
+    $this->nose = $attributes['nose'];
+
+    $this->unusedstatpoints = $attributes['unusedstatpoints'];
+    $this->level = $attributes['level'];
+    $this->currentexperience = $attributes['currentexperience'];
+    $this->maxexperience = $attributes['maxexperience'];
+    $this->currenthealth = $attributes['currenthealth'];
+    $this->maxhealth = $attributes['maxhealth'];
+    $this->strength = $attributes['strength'];
+    $this->defense = $attributes['defense'];
+    $this->speed = $attributes['speed'];
+    $this->agility = $attributes['agility'];
+    $this->accuracy = $attributes['accuracy'];
+
+    $this->weapons = $attributes['weapons'];
+    $this->currentweapon = $attributes['currentweapon'];
+    $this->shields = $attributes['shields'];
+    $this->currentshield = $attributes['currentshield'];
+    $this->inventory = $attributes['inventory'];
+    $this->potionholster = $attributes['potionholster'];
+    $this->gold = $attributes['gold'];
+    $this->maxgold = $attributes['maxgold'];
+
+    $this->dead = $attributes['dead'];
+  }
+
+  /*Getter and Setter as found on SO: http://stackoverflow.com/questions/4478661/getter-and-setter */
   public function __get($property) {
     if (property_exists($this, $property)) {
       return $this->$property;
@@ -55,6 +90,79 @@ class Character
     }
 
     return $this;
+  }
+
+  /**
+  * Get by character ID
+  * Characters should be returnable via a character ID.
+  * @return A Character object
+  */
+  public static function getByCharacterID($characterID) {
+    
+  }
+
+  /**
+  * Attack
+  * The amount of damage taken by a blow from an enemy will vary
+  * depending on type differences, the character's stats, the
+  * monster's stats, and the equipment that the character and enemy own
+  * wield.
+  *
+  * This damage may differ from the monster damage calculation
+  *
+  * @return an array that could contain the following keys:
+  * - damage:    An array with keys:
+  *   - points:  the amount of damage dealt. Ranges from 0 to n, where n
+  *              is the max health of the enemy.
+  *   - dead:    TRUE if enemy is dead
+  * - condition: An array with keys: 
+  *   - type:    character can inflict a condition of poison, sleep, slow,
+  *              blind and others. FALSE if nothing.
+  *   - applied: If the enemy is resistant or the condition didn't apply,
+  *              this will be FALSE.
+  * - miss:      If the character misses, this is TRUE.
+  * - dodge:     If the enemy dodges, this is TRUE.
+  */
+  public function attack($enemy, $damager) {
+    $random = mt_rand(1,100);
+    if ($random < accuracy) {
+      $random = mt_rand(1,100);
+      if ($random > $enemy->agility) {
+        $damage    = $damager->calculateDamage($this, $enemy);
+        $condition = $damager->inflictCondition();
+        $applied   = $enemy->takeCondition($condition);
+        $dead      = $enemy->takeDamage($damage);
+        $result    = array('damage' => array('points' => $damage, 'dead' => $dead), 'condition' => array('type' => $condition, 'applied' => $applied));
+      } else {
+        $result = array('damage' => 0, 'dodge' => TRUE);
+      }
+    } else {
+      $result = array('damage' => 0, 'miss' => TRUE);
+    }
+    return $result;
+  }
+
+  /**
+  * Take Condition
+  * Check if a condition can be applied to character.
+  * @return TRUE if applied, FALSE if not
+  */
+  public function takeCondition($condition) {
+    if(in_array($condition, $this->resist) || in_array($condition, $shields[$currentshield]->resist)) {
+      return FALSE;
+    } else {
+      return TRUE;
+    }
+  }
+
+  /**
+  * Take Damage
+  * Take a given amount of damage from $currenthealth.
+  * @return TRUE if dead, FALSE if still alive
+  */
+  public function takeDamage($damage) {
+    $currenthealth -= $damage;
+    return ($currenthealth<=0);
   }
 
   /**
@@ -82,12 +190,30 @@ class Character
   * When a character gains a level, a couple of things happen:
   * Level increases by 1
   * Experience points until next level are recalculated
+  * Any carry-over experience points are set to the current experience points
   * Character gains Attribute points to spend as they please
   * - Attributes are determined logarithmically
   */
-  public function gainLevel() {
+  public function gainLevel($carryover) {
     $this->level++;
-    $this->maxexperience = calculateExperience();
-    $this->unusedstatpoints = calculateAttributePoints();
+    $this->currentexperience = $carryover;
+    $this->maxexperience     = calculateExperience();
+    $this->unusedstatpoints += calculateAttributePoints();
+  }
+
+  /**
+  * Gain Experience
+  * When a character kills an enemey, they receive exeperience points.
+  * If the amount of experience points they gain causes their current
+  * experience points to excede their max, they will level. Extra experience
+  * points will roll over. This will occur until a character finally
+  * has less current experience points than their max
+  */
+  public function gainExperience($points) {
+    $this->currentexperience+$points;
+    while ($this->currentexperience >= $this->maxexperience) {
+      $carryover = $this->maxexperience - ($this->currentexperience+$points);
+      $this->gainLevel($carryover);
+    }
   }
 }
